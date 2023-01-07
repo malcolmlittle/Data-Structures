77f33ddb01a754633433b1a87b6286339fe3a33a
Malcolm Little <malcolmvlittle@gmail.com>
2023-01-07 14:38:11 -0800

    added comments

--- recursion/sumOfDigits.py
+++ recursion/sumOfDigits.py
@@ -4,10 +4,10 @@
 # f(n) = n % 10 + f(n / 10)
 
 def sumOfDigits(n):
-    assert n >= 0 and int(n) == n, 'The number has to be a positive integer!'
-    if n == 0:
+    assert n >= 0 and int(n) == n, 'The number has to be a positive integer!' # unintentional case - the constraint
+    if n == 0: # base case
         return 0
     else:
-        return int(n % 10) + sumOfDigits(int(n / 10))
+        return int(n % 10) + sumOfDigits(int(n / 10)) # recursive case - the flow
 
 print(sumOfDigits(3200))
