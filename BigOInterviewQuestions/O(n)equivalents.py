64c6a958234809b46ef97f2e8f562295e4c1274c
Malcolm Little <malcolmvlittle@gmail.com>
2023-01-09 19:54:45 -0800

    question 7 - O(n) equivalents

---
+++ BigOInterviewQuestions/O(n)equivalents.py
@@ -0,0 +1,11 @@
+# Which of the following are equivalent to O(n)? Why?
+
+# 1. O(N + P), where P < N/2        --p is less than n therefore nondominant and can be dropped
+# 2. O(2n)      --constant can be dropped
+# 3. O(n + log n)   --log n non dominant can be dropped
+
+
+# 4. O(n + n log n)     --drop n cause nondominant so this is not equal to O(n) and is instead equal to O(n log n)
+# 5. O(n + m)       -- note that a relationship isn't established between n & m so wee need to keep both variables
+
+# 1, 2, & 3
