63abfbfb7f190780a4807eff99151baaed12613b
Malcolm Little <malcolmvlittle@gmail.com>
2023-01-09 19:13:16 -0800

    question 6 - reverse

---
+++ BigOInterviewQuestions/reverse.py
@@ -0,0 +1,11 @@
+# What is the runtime of the below code?
+
+def reverse(array):
+    for i in range(0, int(len(array)/2)):
+        other = len(array) - i - 1
+        temp = array[i]
+        array[i] = array[other]
+        array[other] = temp
+    print(array)
+
+# The runtime is O(n) - linear
