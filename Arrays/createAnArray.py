2e2d484dbdfee90970c2f11f7d91a95003def03a
Malcolm Little <malcolmvlittle@gmail.com>
2023-01-10 18:46:45 -0800

    create an array

---
+++ Arrays/createAnArray.py
@@ -0,0 +1,7 @@
+# import Array module because python doesn't have them built in
+from array import *
+
+array1 = array('i', [1, 2, 3, 4, 5, 6])
+array2 = array('d', [1.3, 1.5, 1.6])
+
+print(array1)
