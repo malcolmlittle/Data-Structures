5a27e8e64ed414e27b97d75732c6bfdcbc7f133f
Malcolm Little <malcolmvlittle@gmail.com>
2023-01-10 19:13:24 -0800

    insertion operations on arrays

--- Arrays/createAnArray.py
+++ Arrays/createAnArray.py
@@ -4,6 +4,13 @@ from array import *
 array1 = array('i', [1, 2, 3, 4, 5, 6])
 array2 = array('d', [1.3, 1.5, 1.6])
 
-print(array1)
+# Insertion at the beginning of an array is O(n) linear time complexity
+array1.insert(0, 23)
 
-# creating an array is an O(1) constant time operation
+# Insertion at the end of an array is O(n) constant time because we provide index and no elements need to shift
+array2.insert(3, 2.0)
+
+# Insertion to full array is O(n) linear time complexity because we have to create new array and move our elements to it
+
+print(array1)
+print(array2)
